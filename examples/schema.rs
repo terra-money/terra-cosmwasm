32c53d7cdbe26bcef05076254158abddb6703dfe
yys <sw.yunsuk@gmail.com>
2020-07-28 17:06:12 +0900

    update cosmwasm to v0.10.0-alpha2

--- examples/schema.rs
+++ examples/schema.rs
@@ -2,7 +2,7 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use terra_bindings::{
+use terra_cosmwasm::{
     SwapResponse, TaxCapResponse, TaxRateResponse, TerraMsg, TerraMsgWrapper, TerraQuery,
     TerraQueryWrapper,
 };
